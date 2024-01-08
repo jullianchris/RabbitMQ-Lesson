90e3c74dae9048f722a1e6514d8ebfe948fe8a2e
jullianchris <jullianchristopher6@gmail.com>
2024-01-09 00:21:06 +0530

    baisic implementation rabbitmq producer pass message to consumer

---
+++ RabbitMQProducer/RabbitMQProducer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(
+    "demo-queue-leason",
+    durable: true,
+    exclusive: false ,
+    autoDelete: false,
+    arguments: null);
+Console.WriteLine("started!");
+var message = new { Name = "Producer", Message = "Hello!" };
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+channel.BasicPublish("", "demo-queue-leason", null, body);
+Console.ReadLine();
